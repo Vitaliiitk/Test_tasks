8056b0ae4e37cb5b7eb7ff530db215bc9c1e3dce
Vitalii Tkachenko <ilikecsgo@gmail.com>
2024-09-16 02:47:40 +0300

    Added a test task

--- test_task_bitsOrchestra/test_task_bitsOrchestra/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace test_task_bitsOrchestra.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
