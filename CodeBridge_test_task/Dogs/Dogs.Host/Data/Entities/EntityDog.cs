5282d5cc33576107a85e45fbbabbcdef9ae7240a
Vitalii Tkachenko <ilikecsgo@gmail.com>
2024-11-03 18:47:25 +0200

    Add CodeBridge test task files

---
+++ CodeBridge_test_task/Dogs/Dogs.Host/Data/Entities/EntityDog.cs
@@ -0,0 +1,12 @@
+﻿namespace Dogs.Host.Data.Entities
+{
+	public class EntityDog
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+		public string Color { get; set; } = null!;
+		public double TailLength { get; set; }
+		public double Weight { get; set; }
+	}
+}
